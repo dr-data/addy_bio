06e625197977a682532e6ea39fbe4ef7fe5518ec
searchableguy <me@searchableguy.com>
2022-01-09 19:54:53 +0530

    feat: Add Navigation component, some text to the index page.

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,12 @@
+export function Navigation() {
+  return (
+    <div className="sticky top-0 z-30 bg-opacity-50 h-[74px] bg-white backdrop-filter backdrop-blur  firefox:bg-opacity-90">
+      <div className="container flex justify-between items-center mx-auto px-4 max-w-screen-sm h-[70px]">
+        <div>
+          <p className="text-xl font-bold">Addy </p>
+        </div>
+        <nav className="flex gap-2"></nav>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,21 @@ import type { NextPage } from "next";
 import { CenteredColumn } from "@components/Layouts";
 import { routes } from "@config/routes";
 import { NextSeo } from "next-seo";
-
+import { Navigation } from "@components/Navigation"
 const Home: NextPage = () => {
   return (
     <div>
+      <Navigation />
       <CenteredColumn className="z-10 p-4 pt-12">
         <NextSeo
           title={routes.home.seo.title}
           description={routes.home.seo.description}
           openGraph={routes.home.seo.openGraph}
         />
+
+        <div>
+          <h1 className="font-font-extrabold text-6xl"> Share all your web3 wallets in a bio link. </h1>
+        </div>
       </CenteredColumn>
     </div>
   );
